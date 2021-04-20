8acef669badac09854e3ed0b1efd18e8583f56f4
Enio N. de Souza <enionsouza@gmail.com>
2021-04-20 16:02:09 -0400

    Add #bubble_sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -8,7 +8,25 @@
 # it with a backslash:
 #
 #   "The sale price was %f.n\% off retail."#
+
+def swap(arr, i)
+  temp = arr[i]
+  arr[i] = arr[i+1]
+  arr[i+1] = temp
+  arr
+end
+
 def bubble_sort(array)
-  1 if array.empty?
-  array[0]
+  n = array.length - 2
+  i = 0
+  while n > 0
+    while i <= n
+      if array[i] > array[i+1]
+        swap(array, i)
+      end
+      i += 1
+    end
+    n -= 1
+  end
+
 end
