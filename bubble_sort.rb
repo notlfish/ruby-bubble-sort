87ea46d5b446746103e41b1f5e4b063c1d05e55a
Lucas Ferrari Soto <lucasmfsoto@gmail.com>
2021-04-20 14:56:45 -0300

    Add template code and TomDoc documentation for #bubble_sort

---
+++ bubble_sort.rb
@@ -0,0 +1,14 @@
+# Public: Format some data with the given format. Possible format
+# identifiers include:
+#
+# %i   - Output the Integer i.
+# %f.n - Output a Float f with n decimal places rounded.
+#
+# The format String may include any text. To escape a percent sign, prefix
+# it with a backslash:
+#
+#   "The sale price was %f.n\% off retail."#
+def bubble_sort(array)
+  1 if array.empty?
+  array[0]
+end
